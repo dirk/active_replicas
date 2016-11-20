3b30579e2d4bf52afdb9e60e86a97b37dfe36b6f
Dirk Gadsden <dirk@esherido.com>
2016-11-20 00:11:09 -0800

    Improve example application

--- example/app.rb
+++ example/app.rb
@@ -1,14 +1,22 @@
+# Based off an example single-file Rails application:
+#   https://gist.github.com/shawndrost/1629835
+
 require 'rails/all'
 
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'active_replicas'
 
 require 'logger'
-Rails.logger = Logger.new STDOUT
+Rails.logger = ActiveRecord::Base.logger = Logger.new STDOUT
 
 class Tester < Rails::Application
   config.secret_key_base = 'abc123'
 end
+
+ActiveReplicas::Railtie.hijack_active_record primary: { url: 'mysql2://root@localhost/active_replicas' },
+                                             replicas: {
+                                               replica0: { url: 'mysql2://root@localhost/active_replicas' }
+                                             }
 
 # require 'sqlite3'
 # database_file = File.join(File.dirname(__FILE__), 'test.sqlite3')
@@ -21,7 +29,12 @@ end
 class User < ActiveRecord::Base
 end
 
-ActiveReplicas::Railtie.hijack_active_record primary: { url: 'mysql2://root@localhost/active_replicas' },
-                                             replicas: {
-                                               replica0: { url: 'mysql2://root@localhost/active_replicas' }
-                                             }
+class UsersController < ActionController::Base
+  def index
+    render text: User.all.map(&:email).join(',')
+  end
+end
+
+Tester.routes.draw do
+  root to: 'users#index'
+end
