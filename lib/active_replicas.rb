efd3acbd462170d66220b27e82923c527abb2dea
Dirk Gadsden <dirk@esherido.com>
2016-11-20 21:58:39 -0800

    Move `ConnectionHandler` into module specific to ActiveRecord version

--- lib/active_replicas.rb
+++ lib/active_replicas.rb
@@ -1,11 +1,16 @@
 require 'active_replicas/version'
-require 'active_replicas/connection_handler'
 require 'active_replicas/log_subscriber'
 require 'active_replicas/proxying_connection'
 require 'active_replicas/proxying_connection_pool'
 
-module ActiveReplicas
-  # Your code goes here...
+if defined? ActiveRecord
+  version = ActiveRecord::VERSION::MAJOR
+
+  if version == 4
+    require 'active_replicas/rails4/connection_handler'
+  else
+    raise "Unsupported ActiveRecord version: #{version}"
+  end
 end
 
-require 'active_replicas/railtie' if defined?(Rails)
+require 'active_replicas/railtie' if defined? Rails
---
+++ lib/active_replicas/rails4/connection_handler.rb
@@ -0,0 +1,52 @@
+require 'concurrent/map'
+
+module ActiveReplicas
+  module Rails4
+    # Wraps around Rails' `ActiveRecord::ConnectionAdapters::ConnectionHandler`
+    # to provide proxy wrappers around requested connections.
+    class ConnectionHandler
+      def initialize(proxy_configuration:, delegate: nil, overrides: nil)
+        @proxy_configuration = proxy_configuration
+        @delegate            = delegate
+        @overrides           = Set.new(overrides || [])
+
+        # Each process will get its own map of connection keys to database
+        # connection instances.
+        @process_to_connection_pool = Concurrent::Map.new
+      end
+
+      def establish_connection(owner, _spec)
+        raise "ActiveReplicas cannot establish connection for #{owner.name}"
+      end
+
+      def clear_active_connections!
+        proxying_connection_pool.release_connection
+      end
+
+      # Cribbed from:
+      #   https://github.com/rails/rails/blob/4-2-stable/activerecord/lib/active_record/connection_adapters/abstract/connection_pool.rb#L568
+      def retrieve_connection(klass)
+        pool = retrieve_connection_pool klass
+        raise ConnectionNotEstablished, "No connection pool for #{klass}" unless pool
+        conn = pool.connection
+        raise ConnectionNotEstablished, "No connection for #{klass} in connection pool" unless conn
+        conn
+      end
+
+      def retrieve_connection_pool(klass)
+        proxying_connection_pool
+      end
+
+      def connected?(klass)
+        pool = retrieve_connection_pool klass
+        pool && pool.connected?
+      end
+
+      def proxying_connection_pool
+        @process_to_connection_pool[Process.pid] ||= ProxyingConnectionPool.new(@proxy_configuration)
+      end
+    end
+  end
+
+  ConnectionHandler = Rails4::ConnectionHandler
+end
