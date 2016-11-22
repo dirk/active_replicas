49c541a5d5d05a1b62a7647ae240b7209a4f3b6e
Dirk Gadsden <dirk@esherido.com>
2016-11-21 16:49:25 -0800

    Update RSpec configuration

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,3 +4,9 @@ require 'rubygems'
 require 'active_record'
 
 require 'active_replicas'
+
+RSpec.configure do |config|
+  config.mock_with :rspec do |mocks|
+    mocks.syntax = [ :expect, :should ]
+  end
+end
